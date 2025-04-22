8bda50858d04abcf12ac97efbab2f7a3e20cec7d
BryanRO-SpTech <bryan.grocha@sptech.school>
2025-04-22 18:31:49 -0300

    test-boto3

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import boto3
+import json
 
 app = Flask(__name__)
 
@@ -7,9 +8,12 @@ s3 = boto3.resource('s3')
 
 @app.route('/store/s3/raw')
 def hello_world():
-    for bucket in s3.buckets.all():
-        print(bucket.name)
+    buckets = dict()
 
+    for bucket in s3.buckets.all():
+        buckets[bucket.name] = bucket
+
+    return json.dumps(buckets)
 
 if __name__ == '__main__':
     app.run()
