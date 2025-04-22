e07c537a6e7ebe354723dc9883ac47b681f046a9
BryanRO-SpTech <bryan.grocha@sptech.school>
2025-04-22 18:35:11 -0300

    test-boto3

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ s3 = boto3.resource('s3')
 
 @app.route('/store/s3/raw')
 def hello_world():
-    buckets = dict()
+    buckets = []
 
     for bucket in s3.buckets.all():
-        buckets[bucket.name] = bucket
+        buckets.append(bucket.name)
 
     return jsonify(buckets)
 
